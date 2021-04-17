8f8f645151ab055711eae172fb158188e6d5fd99
Iskander <kalim.iskn@gmail.com>
2021-04-17 14:40:45 +0300

    Initial

---
+++ src/main/java/ru/kpfu/itis/iskander/mysound/MySoundApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.iskander.mysound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kpfu/itis/iskander/mysound/MySoundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.iskander.mysound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
